c4983c04c50a7292fc6eea31ae920441e3a7823d
moyasiginko <mrsiyam61@gmail.com>
2024-12-19 15:35:17 +0600

    refactore code best practice

--- Scenes/Characters/Character.gd
+++
@@ -1,7 +0,0 @@
-extends Node3D
-class_name Character
-
-@onready var collision_shape: CollisionShape3D = $CollisionShape3D
-
-func get_collision_shape() -> CollisionShape3D:
-	return collision_shape
